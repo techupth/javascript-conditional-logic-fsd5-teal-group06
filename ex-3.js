33b91ca5058e73204341040d836de26073f3c580
Tai <5506726tai@gmail.com>
2023-11-30 15:36:40 +0700

    Feat ex finish

--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,6 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+lightBulbStatus === "On" || lightBulbStatus === "Off"
+  ? console.log(`Light bulb is On. ${lightBulbStatus}`)
+  : console.log(`Please choose the correct input (On/Off) `);
--- ex-4.js
+++ ex-4.js
@@ -3,3 +3,19 @@
 let lightBulbStatus = "On";
 
 //Start coding here
+let light;
+switch (lightBulbStatus) {
+  case "On":
+    light = "Light bulb is On.";
+    break;
+  case "Off":
+    light = "Light bulb is Off.";
+    break;
+  case "Broken":
+    light = "Light bulb is Broken.";
+    break;
+  default:
+    light = "Please choose the correct input (On/Off/Broken)";
+}
+
+console.log(light);
